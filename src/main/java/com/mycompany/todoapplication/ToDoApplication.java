3a7e798dda5931748f53fd72b36cc9b9dee7bbb3
AmandaAmarante <amanda.amarante.santos@gmail.com>
2023-07-21 13:49:28 -0300

    firdt commit

---
+++ src/main/java/com/mycompany/todoapplication/ToDoApplication.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.todoapplication;
+
+import controller.ProjectController;
+import java.sql.Connection;
+import model.Project;
+import util.ConnectionFactory;
+
+
+public class ToDoApplication {
+
+    public static void main(String[] args) {
+        
+//        ProjectController projectController = new ProjectController();
+//        
+//        Project project = new Project();
+//        project.setName("Projeto teste");
+//        project.setDescription("description");
+//        projectController.save(project);
+        
+        
+        
+//        Connection c = ConnectionFactory.getConnection();
+//        ConnectionFactory.closeConnection(c);
+        
+    }
+}
